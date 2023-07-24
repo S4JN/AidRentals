cc705f96e55f796867c1f77d893aadf482263d36
S4JN <srajanacharya09@gmail.com>
2023-07-25 00:33:34 +0530

    font

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Featured from '../components/Featured/Featured'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+
+        <Featured />
+      </div>
+      <Footer />
     </div>
   )
 }
