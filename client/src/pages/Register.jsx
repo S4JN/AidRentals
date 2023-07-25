c4d864740eafca7f0cdf0e4a10c2bbc06e9213fa
S4JN <srajanacharya09@gmail.com>
2023-07-25 11:30:12 +0530

    registerfix

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     if (!role || !email || !password) {
       console.log("enter all fields");
       console.log(role, email, password);
-     
+
 
     } else {
 
@@ -40,137 +40,181 @@ const Register = () => {
 
   return (
     <>
-   
-          <Grid container component="main" sx={{ height: '100vh' }}>
-            <CssBaseline />
-            <Grid
-              item
-              xs={false}
-              sm={4}
-              md={7}
-              sx={{
-                backgroundImage: `url(${loginImage})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundColor: (t) =>
-                  t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-              <Box
-                sx={{
-                  my: 4,
-                  mx: 4,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                  <LockOutlined />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                  Sign Up
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }} >
-                  <Dabba>
 
-                    <RadioGroup aria-label="role" name="role" value={selectedRole} onChange={handleRoleChange}>
-                      <FormControlLabel value="Customer" control={<Radio />} label="Customer" />
-                      {/* <FormControlLabel value="admin" control={<Radio />} label="Admin" /> */}
-                      <FormControlLabel value="organisation" control={<Radio />} label="Organisation" />
-                      <FormControlLabel value="Compunder" control={<Radio />} label="Compunder" />
-                    </RadioGroup>
-                  </Dabba>
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="name"
-                    label="Enter Your Name"
-                    name="name"
-                    autoComplete="name"
-                    autoFocus
-                  />
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                  />
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="cpassword"
-                    label="Confirm Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="contact"
-                    label="Contact Number"
-                    type="tel"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                  {/* <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              /> */}
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Sign Up
-                  </Button>
-                  <Grid container>
-                    <Grid item xs>
-                      {/* <Link href="#" variant="body2">
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${loginImage})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 4,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> */}
+            {/* <LockOutlined /> */}
+            {/* </Avatar> */}
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }} >
+              <Dabba>
+
+                <RadioGroup aria-label="role" name="role" value={selectedRole} onChange={handleRoleChange}>
+                  <FormControlLabel value="Customer" control={<Radio />} label="Customer" />
+                  {/* <FormControlLabel value="admin" control={<Radio />} label="Admin" /> */}
+                  <FormControlLabel value="organisation" control={<Radio />} label="Organisation" />
+                  <FormControlLabel value="Compunder" control={<Radio />} label="Compunder" />
+                </RadioGroup>
+              </Dabba>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Enter Your Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+
+              <PasswordCont>
+                <TextField
+                  margin="normal"
+                  required
+
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <TextField
+                  margin="normal"
+                  required
+
+                  name="cpassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+
+              </PasswordCont>
+
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="contact"
+                label="Contact Number"
+                type="tel"
+                id="contact"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+
+                id="address"
+                label="Address"
+                name="address"
+                autoComplete="address"
+                autoFocus
+              />
+
+              <PasswordCont>
+
+                <Cont
+                  margin="normal"
+                  required
+                  id="city"
+                  label="City"
+                  name="city"
+                  autoComplete="city"
+                  autoFocus
+                />
+                <Cont
+                  margin="normal"
+                  required
+                  id="state"
+                  label="State"
+                  name="state"
+                  autoComplete="state"
+                  autoFocus
+                />
+                <Cont
+                  margin="normal"
+                  required
+                  id="zipCode"
+                  label="Zip Code"
+                  name="zipCode"
+                  autoComplete="zipCode"
+                  autoFocus
+                />
+
+              </PasswordCont>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  {/* <Link href="#" variant="body2">
                     Forgot password?
                   </Link> */}
-                    </Grid>
-                    <Grid item>
-                      <RegisterTo variant="body2" >
-                        Already have an account? <Link href='/login'>Sign In</Link>
-                      </RegisterTo>
-                    </Grid>
-                  </Grid>
-                  <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
-                    {'Copyright © '}
-                    <Link color="inherit" href="/AboutUs">
-                      AidRentals
-                    </Link>{' '}
-                    {new Date().getFullYear()}
-                    {'.'}
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
-          </Grid>
- 
-     
+                </Grid>
+                <Grid item>
+                  <RegisterTo variant="body2" >
+                    Already have an account? <Link href='/login'>Sign In</Link>
+                  </RegisterTo>
+                </Grid>
+              </Grid>
+              <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
+                {'Copyright © '}
+                <Link color="inherit" href="/AboutUs">
+                  AidRentals
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+
+
     </>
   );
 };
@@ -200,3 +244,19 @@ const Dabba = styled(Box)`
     flex-direction: row;
   }
 `;
+
+const PasswordCont = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  
+`
+
+const Cont = styled(TextField)`
+  
+  width: 30%;
+
+
+`
