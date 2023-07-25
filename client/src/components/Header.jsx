c2f0fff7987abc5ff92e4447ae0cddb7b89fca24
S4JN <srajanacharya09@gmail.com>
2023-07-26 00:19:00 +0530

    fonts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,3 @@
-
 import "./header.css";
 import { DateRange } from "react-date-range";
 import { useState } from "react";
@@ -6,80 +5,79 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
 
 const Header = ({ type }) => {
+  const [activeButton, setActiveButton] = useState('headerButton1');
+  const navigate = useNavigate();
+  
+  const handleOption = (name, operation) => {
+    // Implementation of the handleOption function
+  };
 
-    const [activeButton, setActiveButton] = useState('headerButton1');
-    const navigate = useNavigate();
-    const handleOption = (name, operation) => {
-      
-    };
-
-    const handleSearch = () => {
-        
-    };
+  const handleSearch = () => {
+    // Implementation of the handleSearch function
+  };
     
   const handleButtonClick = (headerButtonId) => {
     setActiveButton(headerButtonId);
   };
 
-    return (
-        <div className="header">
-            <div
-                className={
-                    type === "list" ? "headerContainer listMode" : "headerContainer"
-                }
-            >
-                <div className="headerList">
-                    <div  className={`headerListItem ${activeButton === 'headerButton1' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton1')}>
-                        <span>Compunder</span>
-                    </div>
-                    <div  className={`headerListItem ${activeButton === 'headerButton2' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton2')}>
-                        <span>Compunder</span>
-                    </div>
-                    <div  className={`headerListItem ${activeButton === 'headerButton3' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton3')}>
-                        <span>Compunder</span>
-                    </div>
-                    <div  className={`headerListItem ${activeButton === 'headerButton4' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton4')}>
-                        <span>Compunder</span>
-                    </div>
-                    <div  className={`headerListItem ${activeButton === 'headerButton5' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton5')}>
-                        <span>Compunder</span>
-                    </div>
-                </div>
-                {type !== "list" && (
-                    <>
-                        <h1 className="headerTitle">
-                            Help because pata nhi kis roop m akr naryan mil jyega.
-                        </h1>
-                        <p className="headerDesc">
-                            Rent your medical instruments for the needy, keep prices low.
-                            db ki ma ka bhosda
-                        </p>
-
-                        <div className="headerSearch">
-
-                            <div className="headerSearchItem">
-                                <input
-                                    type="text"
-                                    placeholder="Whats your need?"
-                                    className="headerSearchInput"
-                                    onChange={(e) => setDestination(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="headerSearchItem">
-                                <headerButton className="headerBtn" onClick={handleSearch}>
-                                    Search
-                                </headerButton>
-                            </div>
-
-                        </div>
-                    </>
-                )}
-            </div>
+  return (
+    <div className="header">
+      <div
+        className={
+          type === "list" ? "headerContainer listMode" : "headerContainer"
+        }
+      >
+        <div className="headerList">
+          <div className={`headerListItem ${activeButton === 'headerButton1' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton1')}>
+            <HomeIcon /> <span>  Compunder</span>
+          </div>
+          {/* Additional headerList items go here */}
+          <div className={`headerListItem ${activeButton === 'headerButton3' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton3')}>
+             <span>  Compunder</span>
+          </div>
+          <div className={`headerListItem ${activeButton === 'headerButton4' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton4')}>
+             <span>  Compunder</span>
+          </div>
+          <div className={`headerListItem ${activeButton === 'headerButton5' ? 'active' : ''}`} onClick={() => handleButtonClick('headerButton5')}>
+             <span>  Compunder</span>
+          </div>
         </div>
-    );
+
+        {type !== "list" && (
+          <>
+            <h1 className="headerTitle">
+              Help because pata nhi kis roop m akr naryan mil jyega.
+            </h1>
+            <p className="headerDesc">
+              Rent your medical instruments for the needy, keep prices low.
+              db ki ma ka bhosda
+            </p>
+
+            <div className="headerSearch">
+              <div className="headerSearchItem">
+                <input
+                  type="text"
+                  placeholder="Whats your need?"
+                  className="headerSearchInput"
+                  onChange={(e) => setDestination(e.target.value)}
+                />
+              </div>
+
+              <div className="headerSearchItem">
+                {/* Using a regular HTML button instead of an undefined headerButton */}
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,18 +3,39 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Featured from '../components/Featured/Featured'
+import { Box, styled, Typography } from '@mui/material'
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
-      <div className='homeContainer'>
+      <Container className='homeContainer'>
         <Featured />
-      </div>
+        <div>
+          <Heading variant="h2">Browse the inventory</Heading>
+        </div>
+      </Container>
       <Footer />
     </div>
   )
 }
 
 export default Home
+
+const Container = styled(Box)`
+${'' /* margin-top: 50px; */}
+  display: flex;
+  flex-direction: column;
+   align-items: start;
+   gap: 30px;
+`
+
+const Heading = styled(Typography)`
+  font-weight: bold;
+  font-size: 32px;
+  margin-top: 5%;
+  margin-left: 200px;
+`
+
