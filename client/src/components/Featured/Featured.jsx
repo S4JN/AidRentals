48d7e3d185f833c792c88bdb79289b6c7dc2b770
S4JN <srajanacharya09@gmail.com>
2023-07-25 15:47:11 +0530

    g

--- client/src/components/Featured/Featured.jsx
+++ client/src/components/Featured/Featured.jsx
@@ -1,40 +1,42 @@
 import "./featured.css";
+import bedImg from "../../assets/bed.jpg"
+import oxygen from "../../assets/oxygen.jpg"
+import nurse from "../../assets/nurse.jpg"
+
 
 const Featured = () => {
   return (
     <div className="featured">
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          src={bedImg}
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Indore</h1>
-          <h2>123 items</h2>
+          <h1>Beds</h1>
+          
         </div>
       </div>
       
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          src={oxygen}
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Indore</h1>
-          <h2>533 items</h2>
+          <h1>Oxygen Cylinders</h1>
         </div>
       </div>
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          src={nurse}
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Indore</h1>
-          <h2>532 items</h2>
+          <h1>Services</h1>
         </div>
       </div>
     </div>
