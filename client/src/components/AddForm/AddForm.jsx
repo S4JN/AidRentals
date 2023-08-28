cf9d971eca7e95786b1f7986fd84ea8450daad24
S4JN <srajanacharya09@gmail.com>
2023-08-28 20:31:58 +0530

    one way

--- client/src/components/AddForm/AddForm.jsx
+++ client/src/components/AddForm/AddForm.jsx
@@ -1,39 +1,48 @@
-import { useState } from 'react';
-import "./addForm.css"
-import axios from 'axios'
+import React, { useState } from 'react';
+import FileBase from 'react-file-base64';
 import { useUserContext } from '../../context/UserContext';
+import { useEffect } from 'react';
 
-const AddForm = ({ setShowForm, showForm,handleFormClose }) => {
+import axios from "axios";
+
+
+const AddForm = ({ setShowForm }) => {
+
   const { user } = useUserContext();
-
-
+  const _id = user._id;
 
   const [formData, setFormData] = useState({
     name: '',
-    file: null,
+    owner: _id,
     description: '',
     rentalPrice: '',
     life: '',
-    isRented: false,
     tags: '',
-    rating: '3',
+    image: '',
   });
 
-  const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: type === 'checkbox' ? checked : value,
-      
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
     }));
   };
-  
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission
-    // setShowForm(false)
-    console.log(formData);
+  const handleImageChange = (base64) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      image: base64,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+    console.log(formData.image);
+
+    // Here you can implement the logic to send the form data to the server or perform any other actions.
+
     try {
 
       const config = {
@@ -41,9 +50,10 @@ const AddForm = ({ setShowForm, showForm,handleFormClose }) => {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       };
-      
+
       const { data } = axios.post("http://localhost:8000/api/v1/inventory/add", formData, config);
-      console.log(data?.success);
+      console.log(data);
+
     } catch (error) {
       console.log(error);
     }
@@ -51,266 +61,88 @@ const AddForm = ({ setShowForm, showForm,handleFormClose }) => {
 
   };
 
-  const handleClick = () => {
-    handleFormClose();
-  };
-  
-
-
   return (
-    <div className='box'>
-        <button className='closebtn' onClick={handleClick}>X</button>
-      <form onSubmit={handleSubmit} className='formContainer'>
-        <label>
-          Name:
+    <div style={{ backgroundColor: "white", padding: "30px" }}>
+      <h2>Add Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
           <input
             type="text"
             name="name"
             value={formData.name}
-            onChange={handleChange}
-          // required
+            onChange={handleInputChange}
+            required
           />
-        </label>
-
-       
-        <label>
-          Images
-          <input
-            type="file"
-            name="file"
-            onChange={handleChange}
-          />
-        </label>
-
-        <label>
-          Description:
+        </div>
+        <div>
+          <label>Description:</label>
           <textarea
             name="description"
             value={formData.description}
-            onChange={handleChange}
-          // required
+            onChange={handleInputChange}
+            required
           />
-        </label>
-
-        <label>
-          Rental Price:
+        </div>
+        <div>
+          <label>Price:</label>
           <input
-            type="text"
+            type="number"
             name="rentalPrice"
             value={formData.rentalPrice}
-            onChange={handleChange}
-          // required
+            onChange={handleInputChange}
+            required
           />
-        </label>
-
-        <label>
-          Life:
+        </div>
+        <div>
+          <label>Life:</label>
           <input
             type="text"
             name="life"
             value={formData.life}
-            onChange={handleChange}
+            onChange={handleInputChange}
+            required
           />
-        </label>
-
-        <label>
-          Is Rented:
-          <input
-            type="checkbox"
-            name="isRented"
-            checked={formData.isRented}
-            onChange={handleChange}
-          />
-        </label>
-
-        <label>
-          Tags (comma-separated):
+        </div>
+        <div>
+          <label>Tags:</label>
           <input
             type="text"
             name="tags"
             value={formData.tags}
-            onChange={handleChange}
-          // required
+            onChange={handleInputChange}
+            required
           />
-        </label>
+        </div>
 
-        <label>
-          Rating:
-          <select name="rating" value={formData.rating} onChange={handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </label>
+        <div>
+          <label>Image:</label>
+          {/* <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            required
+          /> */}
 
-        <button type="submit">Submit</button>
+          <div>
+            <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) => handleImageChange(base64)}
+            />
+          </div>
+
+
+        </div>
+
+
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default AddForm;
-
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const InventoryForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     owner: '',
-//     description: '',
-//     rentalPrice: '',
-//     life: '',
-//     isRented: false,
-//     tags: '',
-//     rating: '3',
-//     images: [],
-//   });
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const handleImageChange = (event) => {
-//     const imageFiles = event.target.files;
-//     setFormData({ ...formData, images: imageFiles });
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     const data = new FormData();
-//     Object.entries(formData).forEach(([key, value]) => {
-//       if (key === 'images') {
-//         for (let i = 0; i < value.length; i++) {
-//           data.append('images', value[i]);
-//         }
-//       } else {
-//         data.append(key, value);
-//       }
-//     });
-
-    
-
-//     try {
-//       const config = {
-//         headers: {
-//           Authorization: `Bearer ${localStorage.getItem("token")}`,
-//         },
-//       };
-//       await axios.post('http://localhost:8000/api/v1/inventory/add', data,config, {
-//         headers: { 'Content-Type': 'multipart/form-data' },
-        
-//       });
-//       alert('Item added successfully!');
-//       // Reset the form after successful submission
-//       setFormData({
-//         name: '',
-//         owner: '',
-//         description: '',
-//         rentalPrice: '',
-//         life: '',
-//         isRented: false,
-//         tags: '',
-//         rating: '3',
-//         images: [],
-//       });
-//     } catch (error) {
-//       console.error(error);
-//       alert('Error adding the item.');
-//     }
-//   };
-
-//   return (
-//     <div  className='srajan'>
-//       <h2>Add Inventory Item</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>Name:</label>
-//           <input
-//             type="text"
-//             name="name"
-//             value={formData.name}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Owner:</label>
-//           <input
-//             type="text"
-//             name="owner"
-//             value={formData.owner}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Description:</label>
-//           <textarea
-//             name="description"
-//             value={formData.description}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Rental Price:</label>
-//           <input
-//             type="text"
-//             name="rentalPrice"
-//             value={formData.rentalPrice}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Life:</label>
-//           <input
-//             type="text"
-//             name="life"
-//             value={formData.life}
-//             onChange={handleInputChange}
-//           />
-//         </div>
-//         <div>
-//           <label>Tags (comma-separated):</label>
-//           <input
-//             type="text"
-//             name="tags"
-//             value={formData.tags}
-//             onChange={handleInputChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Rating:</label>
-//           <select name="rating" value={formData.rating} onChange={handleInputChange}>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </div>
-//         <div>
-//           <label>Images:</label>
-//           <input
-//             type="file"
-//             name="images"
-//             multiple
-//             onChange={handleImageChange}
-//             required
-//           />
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default InventoryForm;
--- server/controllers/inventorController.js
+++ server/controllers/inventorController.js
@@ -1,69 +1,69 @@
-// const { Inventory } = require("../models/inventorySchema");
-
-// const inventoryController = async (req, res) => {
-//     try {
-
-//         const item = new Inventory(req.body);
-//         await item.save();
-//         return res.status(201).send({
-//             success: true,
-//             message: "Item added",
-//             item
-//         })
-//     }
-//     catch (error) {
-//         console.log(error);
-//         res.status(500).send({
-//             success: false,
-//             message: "idhr error"
-//         });
-//     }
-// };
-
-// module.exports = { inventoryController }
-
-const upload = require("../config/gridFsStorage");
 const { Inventory } = require("../models/inventorySchema");
-// const upload = require("../config/gridFsStorage.js")
-
 
 const inventoryController = async (req, res) => {
-  try {
-    // Upload the images to GridFS and get their filenames
-    await upload.array('images')(req, res, async function (err) {
-      if (err) {
-        console.error(err);
-        return res.status(500).send({
-          success: false,
-          message: "Error uploading images",
+    try {
+
+        const item = new Inventory(req.body);
+        await item.save();
+        return res.status(201).send({
+            success: true,
+            message: "Item added",
+            item
+        })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "idhr error"
         });
-      }
-
-      // Get the image filenames from the request object
-      const imageFilenames = req.files.map(file => file.filename);
-      
-      // Create the inventory item with the image filenames
-      const item = new Inventory({
-        ...req.body,
-        images: imageFilenames,
-      });
-
-      // Save the inventory item to the database
-      await item.save();
-
-      return res.status(201).send({
-        success: true,
-        message: "Item added",
-        item
-      });
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
-      success: false,
-      message: "Error saving the item",
-    });
-  }
+    }
 };
 
-module.exports = { inventoryController };
+module.exports = { inventoryController }
+
+// const upload = require("../config/gridFsStorage");
+// const { Inventory } = require("../models/inventorySchema");
+// // const upload = require("../config/gridFsStorage.js")
+
+
+// const inventoryController = async (req, res) => {
+//   try {
+//     // Upload the images to GridFS and get their filenames
+//     await upload.array('images')(req, res, async function (err) {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).send({
+//           success: false,
+//           message: "Error uploading images",
+//         });
+//       }
+
+//       // Get the image filenames from the request object
+//       const imageFilenames = req.files.map(file => file.filename);
+      
+//       // Create the inventory item with the image filenames
+//       const item = new Inventory({
+//         ...req.body,
+//         images: imageFilenames,
+//       });
+
+//       // Save the inventory item to the database
+//       await item.save();
+
+//       return res.status(201).send({
+//         success: true,
+//         message: "Item added",
+//         item
+//       });
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({
+//       success: false,
+//       message: "Error saving the item",
+//     });
+//   }
+// };
+
+// module.exports = { inventoryController };
