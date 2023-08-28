28b7301d234403e2de9bf43dbed178032541d589
S4JN <srajanacharya09@gmail.com>
2023-08-28 22:46:59 +0530

    fortysix

--- client/src/components/AddForm/AddForm.jsx
+++ client/src/components/AddForm/AddForm.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import FileBase from 'react-file-base64';
 import { useUserContext } from '../../context/UserContext';
 import { useEffect } from 'react';
+import "./addForm.css"
 
 import axios from "axios";
 
-
 const AddForm = ({ setShowForm }) => {
-
   const { user } = useUserContext();
   const _id = user._id;
+
+  const [currentStep, setCurrentStep] = useState(1);
 
   const [formData, setFormData] = useState({
     name: '',
@@ -36,111 +37,136 @@ const AddForm = ({ setShowForm }) => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log('Form Data:', formData);
-    console.log(formData.image);
-
-    // Here you can implement the logic to send the form data to the server or perform any other actions.
 
     try {
-
       const config = {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       };
 
-      const { data } = axios.post("http://localhost:8000/api/v1/inventory/add", formData, config);
+      const { data } = await axios.post("http://localhost:8000/api/v1/inventory/add", formData, config);
       console.log(data);
 
     } catch (error) {
       console.log(error);
     }
-
-
   };
 
+  const nextStep = () => {
+    if (currentStep === 1 && !isStep1Valid()) {
+      alert("fill all the fields first")
+      console.log("fill fields");
+      return;
+    }
+    setCurrentStep(currentStep + 1);
+  };
+
+  const backStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  const isStep1Valid = () => {
+    return (
+      formData.name !== '' &&
+      formData.description !== '' &&
+      formData.rentalPrice !== '' &&
+      formData.life !== '' &&
+      formData.tags !== ''
+    );
+  };
+
+ 
+
   return (
-    <div style={{ backgroundColor: "white", padding: "30px" }}>
-      <h2>Add Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Description:</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Price:</label>
-          <input
-            type="number"
-            name="rentalPrice"
-            value={formData.rentalPrice}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Life:</label>
-          <input
-            type="text"
-            name="life"
-            value={formData.life}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Tags:</label>
-          <input
-            type="text"
-            name="tags"
-            value={formData.tags}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div>
-          <label>Image:</label>
-          {/* <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            required
-          /> */}
-
-          <div>
-            <FileBase
-              type="file"
-              multiple={false}
-              onDone={({ base64 }) => handleImageChange(base64)}
-            />
-          </div>
-
-
-        </div>
-
-
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+    <div style={{ backgroundColor: "white", padding: "30px" }} className='mainBox'>
+      <h2>Add Form - Step {currentStep}</h2>
+      <form onSubmit={currentStep === 2 ? handleSubmit : (e) => e.preventDefault()}>
+        {currentStep === 1 && (
+          <>
+            {/* Step 1 */}
+            <div>
+              <label>Name:</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Description:</label>
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Price:</label>
+              <input
+                type="number"
+                name="rentalPrice"
+                value={formData.rentalPrice}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Life:</label>
+              <input
+                type="text"
+                name="life"
+                value={formData.life}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Tags:</label>
+              <input
+                type="text"
+                name="tags"
+                value={formData.tags}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <button type="button" onClick={nextStep}>Next</button>
+            </div>
+          </>
+        )}
+        {currentStep === 2 && (
+          <>
+            {/* Step 2 */}
+            <div>
+              <label>Image:</label>
+              <div>
+                <FileBase
+                  type="file"
+                  multiple={false}
+                  onDone={({ base64 }) => handleImageChange(base64)}
+                />
+              </div>
+            </div>
+            <div>
+              <button onClick={backStep}>Previous</button>
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </>
+        )}
       </form>
+      <div>
+        <button onClick={() => setShowForm(false)}>Close</button>
+      </div>
     </div>
   );
 };
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,65 +1,65 @@
-// import React, { useState, useEffect } from 'react';
-// import Navbar from '../components/Navbar';
-// import Header from '../components/Header';
-// import "./css/aboutus.css";
-// import axios from 'axios';
-
-// const AboutUs = () => {
-//   const [inventories, setInventories] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch inventories from your API
-
-//     const config = {
-//       headers: {
-//         Authorization: `Bearer ${localStorage.getItem("token")}`,
-//       },
-//     };
-
-
-
-//     axios.get('http://localhost:8000/api/v1/inventory/get', config) // Replace with the appropriate API endpoint
-//       .then(res => {
-//         setInventories(res.data.inventories);
-//         console.log(res.data);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <Header type="list" />
-//       <div className="team-card">
-//         {/* Rest of the About Us content */}
-//       </div>
-//       <div>
-//         <h2>All Inventories</h2>
-//         <ul>
-//           {inventories.map(inventory => (
-//             <li key={inventory.id}>
-//               {/* Display inventory information here */}
-//               {/* For example: */}
-//               <p>{inventory.name}</p>
-//               <p>{inventory.description}</p>
-//               <img src={inventory.image} />
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AboutUs;
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import "./css/aboutus.css";
+import axios from 'axios';
 
 const AboutUs = () => {
-  return (
-    <div>AboutUs</div>
-  )
-}
+  const [inventories, setInventories] = useState([]);
 
-export default AboutUs
+  useEffect(() => {
+    // Fetch inventories from your API
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+
+
+
+    axios.get('http://localhost:8000/api/v1/inventory/get', config) // Replace with the appropriate API endpoint
+      .then(res => {
+        setInventories(res.data.inventories);
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="team-card">
+        {/* Rest of the About Us content */}
+      </div>
+      <div>
+        <h2>All Inventories</h2>
+        <ul>
+          {inventories.map(inventory => (
+            <li key={inventory.id}>
+              {/* Display inventory information here */}
+              {/* For example: */}
+              <p>{inventory.name}</p>
+              <p>{inventory.description}</p>
+              <img src={inventory.image} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
+// import React from 'react'
+
+// const AboutUs = () => {
+//   return (
+//     <div>AboutUs</div>
+//   )
+// }
+
+// export default AboutUs
