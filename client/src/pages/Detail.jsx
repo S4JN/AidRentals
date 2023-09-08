ef8362ef38128c3d5a6ddf91db33d838d4f34f4c
Aditya <adityasugandhi1203@gmail.com>
2023-09-08 16:51:23 +0530

    mergeMain

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -9,7 +9,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useLocation } from 'react-router-dom';
 import Map from '../components/Map/Map';
-
+import RentForm from '../components/RentForm/RentForm'
 const Detail = () => {
 
     const { state } = useLocation();
@@ -106,34 +106,7 @@ const Detail = () => {
                         })}
                     </div>
                     {isFormVisible && (
-                    <div className="overlay">
-                        <div>
-                        <form className="confirmation-form">
-                        <button onClick={toggleForm} className='close'>X</button>
-                        <h1>Final Confirmation</h1>
-                        <h4>Here are the product details</h4>
-                        <ul className="confirmation-list">
-                            <li>
-                                <span className="confirmation-key">Product Name:</span> {state.name}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Price per day:</span> {state.rentalPrice}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Life:</span> {state.life}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Location:</span> {state.address}, {state.city}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">City:</span> {state.city}
-                            </li>
-                        </ul>
-                        
-                        <button type="submit">Submit</button>
-                    </form>
-                    </div>
-                    </div>
+                        <RentForm state={state} toggleForm={toggleForm} />
                      )}
                     <span className="hotelDistance">
                         Excellent location – {state.address}, {state.city}
