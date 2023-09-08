3114ebf122d12bd1312a434270fc2bfd0c6b30db
Aditya <adityasugandhi1203@gmail.com>
2023-09-08 19:02:08 +0530

    to checkout

--- client/src/components/RentForm/RentForm.jsx
+++ client/src/components/RentForm/RentForm.jsx
@@ -1,38 +1,41 @@
 import React from 'react'
 import "./rentForm.css" 
 const RentForm = ({toggleForm,state}) => {
-
+    const uploader=state.owner;
+    console.log(uploader);
     
   return (
     <div className="overlay">
-                        <div>
-                        <form className="confirmation-form">
-                        <button onClick={toggleForm} className='close'>X</button>
-                        <h1>Final Confirmation</h1>
-                        <h4>Here are the product details</h4>
-                        <ul className="confirmation-list">
-                            <li>
-                                <span className="confirmation-key">Product Name:</span> {state.name}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Price per day:</span> {state.rentalPrice}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Life:</span> {state.life}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">Location:</span> {state.address}, {state.owner}
-                            </li>
-                            <li>
-                                <span className="confirmation-key">City:</span> {state.city}
-                            </li>
-                            
-                        </ul>
+        <div>
+            <form className="confirmation-form">
+                <button onClick={toggleForm} className='close'>X</button>
+                    <h1>Final Confirmation</h1>
+                    <h4>Here are the product details</h4>
+                    <ul className="confirmation-list">
+
+                        <li>
+                            <span className="confirmation-key">Product Name:</span> {state.name}
+                        </li>
+                        <li>
+                            <span className="confirmation-key">Price per day:</span> {state.rentalPrice}
+                        </li>
+                        <li>
+                            <span className="confirmation-key">Life:</span> {state.life}
+                        </li>
+                        <li>
+                            <span className="confirmation-key">Location:</span> {state.address}, {state.city}
+                        </li>
+                        <li>
+                            <span className="confirmation-key">City:</span> {state.city}
+                        </li>
+                
+                    </ul>
                         
-                        <button type="submit">Submit</button>
-                    </form>
-                    </div>
-                    </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+
+    </div>
   )
 }
 
