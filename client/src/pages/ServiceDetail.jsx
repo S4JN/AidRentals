a0a4dcf2e1af5e4245429607497085ca98589b13
S4JN <srajanacharya09@gmail.com>
2023-09-17 17:36:07 +0530

    Co-authored-by: Aditya Sugandhi <adityasugandhi1203@gmail.com>

--- client/src/pages/ServiceDetail.jsx
+++ client/src/pages/ServiceDetail.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import { Hourglass, RotatingLines } from 'react-loader-spinner';
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Mail from "../components/MailList/Mail";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "./css/ServiceDetail.css"
+
+import { useLocation } from "react-router-dom";
 
 const ServiceDetail = () => {
   const { state } = useLocation();
@@ -12,20 +15,56 @@ const ServiceDetail = () => {
     <div>
       <Navbar />
       <Header type={"list"} />
-      <div>
-        {/* <Hourglass
-          
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="hourglass-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          colors={['#306cce', '#72a1ed']}
-        /> */}
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <div className="hotelImages">
+            <div className="hotelImgWrapper">
+              <img
+                src={state.pic}
+                alt=""
+                className="hotelImg"
+              />
+            </div>
+            <div className="hotelServiceDetails">
+              <div className="hotelServiceDetailsTexts">
+                <div className="hotelAddress">
+                  {state.specialty.map((tag) => {
+                    return (
+                      <span
+                        key={tag}
+                        style={{
+                          backgroundColor: "#febb02",
+                          fontSize: "0.8rem",
+                          padding: "5px",
+                          color: "#003580",
+                          borderRadius: "9px",
+                        }}
+                      >
+                        {tag}{" "}
+                      </span>
+                    );
+                  })}
+                </div>
+
+                <h1 className="hotelTitle">{state.name}</h1>
+                <span className="hotelDistance">
+                  Excellent location – {state.city}
+                </span>
+                <br />
+                <span className="hotelPriceHighlight">
+                  Rent these items at the cheapest price.
+                </span>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <Mail />
+        <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceDetail
+export default ServiceDetail;
