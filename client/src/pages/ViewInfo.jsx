4c12ac13c8b96ab42cee9b73ea5ede64dce38f99
Aditya <adityasugandhi1203@gmail.com>
2023-10-21 22:47:59 +0530

    pull

--- client/src/pages/ViewInfo.jsx
+++ client/src/pages/ViewInfo.jsx
@@ -5,14 +5,14 @@ import { useUserContext } from '../context/UserContext';
 import Profile from '../components/Profile';
 import Footer from '../components/Footer'
 import Mail from '../components/MailList/Mail';
-import "./css/viewProfile.css"
+import "./css/viewInfo.css"
 import axios from 'axios';
 import SearchItem from '../components/SearchItem/SearchItem';
 
 
 const ViewInfo = () => {
     const { user } = useUserContext();
-    const [items, setItems] = useState([]);
+    const [edit, setEdit] = useState(false);
 
     const address = user?.address;
     const name = user?.name;
@@ -23,41 +23,10 @@ const ViewInfo = () => {
     const zip = user?.zip;
     const id = user?._id;
 
-
-    // const getInv = async () => {
-    //     try {
-    //         if (id) {
-
-    //             const config = {
-    //                 headers: {
-    //                     Authorization: `Bearer ${localStorage.getItem("token")}`,
-    //                 },
-    //             };
-    //             const { data } = await axios.get(`http://localhost:8000/api/v1/inventory/get-user-inv`, {
-    //                 params: {
-    //                     id: id
-    //                 },
-    //                 ...config,
-    //             })
-    //             setItems(data.inventory);
-    //             console.log(data);
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         console.log("error  in fetching khud ke orders");
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     getInv();
-    // }, [id])
-
-
     return (
         <div>
             <Navbar />
             <Header type="list" />
-
             <Profile
                 name={name}
                 address={address}
@@ -66,30 +35,33 @@ const ViewInfo = () => {
                 role={role}
                 city={city}
                 zip={zip}
-
             />
-
-            {/* <h2 className='profile_head'>My Rented Items</h2>
-
-            <div className='vgito'>
-
-                <div className='isko-width'>
-                    {items.map((i) => {
-                        return (
-                            <SearchItem
-                                getInv={getInv}
-                                key={i._id}
-                                item={i}
-                                type={"myProfile"}
-                            />
-                        )
-                    })}
-                </div>
-
-
-
+            <div className='edit-div'>
+                <button className='edit-btn' onClick={(()=>{setEdit(true);})}>Edit Profile</button>
             </div>
- */}
+            {edit&&(
+                
+            <form action="" className='overlay'>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>PhoneNo.:</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <label>city:</label>
+                    <input type="text" />
+                </div>
+                <div className='update'>
+                <label >zipcode:</label>
+                    <input type="number" />
+                </div>
+            
+            </form>
+            )
+            }
             <Mail />
             <Footer />
 
