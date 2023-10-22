6946ba72a3d6be8fe5895ea055da0a745cfe0002
S4JN <srajanacharya09@gmail.com>
2023-10-22 23:52:37 +0530

    verificationIco

--- client/src/components/SearchItem/SearchItem.jsx
+++ client/src/components/SearchItem/SearchItem.jsx
@@ -61,6 +61,7 @@ const SearchItem = ({ item, type ,getInv}) => {
             />
             <div className="siDesc">
                 <h1 className="siTitle">{item.name}</h1>
+                
                 <span className="siDistance">{item.life} yrs old</span>
                 <span className="siTaxiOp">In best Condition</span>
                 <span className="siSubtitle">
